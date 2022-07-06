553f5ce832d11cff1600f8943739064043b421d6
double J, T <50047351+Powering111@users.noreply.github.com>
2022-07-06 21:32:14 +0900

    csv 형식 변경

--- get_climate_data.py
+++ get_climate_data.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+import time
 os.system('pip3 install -r requirements.txt')
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -21,7 +21,7 @@ url = 'https://www.ncdc.noaa.gov/cdo-web/search'
 driver.get(url)
 Select(driver.find_element(By.ID, 'selectedDataset')).select_by_index(2) #데이터 종류 선택 -> 완료
 
-driver.find_element(By.CLASS_NAME, 'noaa-daterange-input').click() #날짜 범위 선택
+driver.find_element(By.CLASS_NAME, 'noaa-daterange-input').click() #날짜 범위 선택 -> 완료
 Select(driver.find_elements(By.CLASS_NAME, 'ui-datepicker-year')[0]).select_by_value('1987')
 Select(driver.find_elements(By.CLASS_NAME, 'ui-datepicker-month')[0]).select_by_index(0)
 driver.find_elements(By.CLASS_NAME, 'ui-state-default')[0].click()
@@ -30,3 +30,17 @@ Select(driver.find_elements(By.CLASS_NAME, 'ui-datepicker-year')[-1]).select_by_
 Select(driver.find_elements(By.CLASS_NAME, 'ui-datepicker-month')[-1]).select_by_index(11)
 driver.find_elements(By.CLASS_NAME, 'ui-state-default')[-1].click()
 
+Select(driver.find_element(By.ID, 'selectedResultType')).select_by_visible_text('States') #검색 방식 선택 -> 완료
+
+states_list = ['ALABAMA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 'COLORADO', 'FLORIDA', 'GEORGIA', 
+ 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA',
+ 'MAINE', 'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 
+ 'NEW JERSEY', 'NEW YORK', 'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OREGON', 'PENNSYLVANIA',
+ 'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON', 
+ 'WEST VIRGINIA', 'WISCONSIN', 'WYOMING']
+
+driver.find_element(By.ID, 'selectedSearchString').send_keys('alabama') #검색어 입력 -> 완료
+
+driver.find_element(By.ID, 'searchSubmit').click() #검색 버튼 클릭 -> 완료
+
+time.sleep(30)
--- parsing_survey_data.py
+++ parsing_survey_data.py
@@ -4,8 +4,19 @@ import pandas as pd
 
 data = pd.read_csv('data/Bee Colony Survey Data by State.csv')
 
+states_list = ['ALABAMA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 'COLORADO', 'FLORIDA', 'GEORGIA', 
+ 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA',
+ 'MAINE', 'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 
+ 'NEW JERSEY', 'NEW YORK', 'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OREGON', 'PENNSYLVANIA',
+ 'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON', 
+ 'WEST VIRGINIA', 'WISCONSIN', 'WYOMING']
+
+for state in states_list:
+    with open(f'data/states/{state}.csv', 'w') as f:
+        f.write('Year, Data Item, Value\n')
+
 for line in data.loc:
-    if line['Period'] == 'MARKETING YEAR':
-        path = f"data/{line['State']}.csv"
+    if line['Period'] == 'MARKETING YEAR' and line['State'] in states_list:
+        path = f"data/states/{line['State']}.csv"
         with open(path, 'a') as f:
             f.write(f"{line['Year']}, {line['Data Item']}, {line['Value'].replace(',', '')}\n")
