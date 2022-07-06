647aaf99d38eaf901e5bddc3cc0e9354881cb0db
double J, T <50047351+Powering111@users.noreply.github.com>
2022-07-06 19:57:49 +0900

    주별로 데이터 완성

--- parsing_survey_data.py
+++ parsing_survey_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 data = pd.read_csv('data/Bee Colony Survey Data by State.csv')
 
 for line in data.loc:
-    path = f"data/{data.loc[line, 'State']}.csv"
-    if data.loc[line, 'Period'] == 'MARKETING YEAR':
-        with open(path, 'w') as f:
-            f.write((data.loc[line, 'Year'], data.loc[line, 'Data Item'], data.loc[line, 'Value']))
+    if line['Period'] == 'MARKETING YEAR':
+        path = f"data/{line['State']}.csv"
+        with open(path, 'a') as f:
+            f.write(f"{line['Year']}, {line['Data Item']}, {line['Value'].replace(',', '')}\n")
