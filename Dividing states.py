db406b424685abc377f630d25e96cc3b98f8bc95
Gabriel-Dropout <lhs0831lkh@naver.com>
2022-07-07 14:13:57 +0900

    주 데이터 다 합침
    ㅇㅇ

--- Dividing states.py
+++ Dividing states.py
@@ -16,7 +16,7 @@ for file in states_list:
     f = open(file, 'r', encoding='utf-8')
     rdr = csv.reader(f)
     years = len(list(rdr))
-    if years == 31:
+    if years == 32: #이거 변동있을수 있음
         correct.append(file[file.index("\\")+1 : file.index("csv")-1])
     else:
         incorrect.append(file[file.index("\\")+1 : file.index("csv")-1])
@@ -25,3 +25,21 @@ for file in states_list:
 print(correct)
 print("else:")
 print(incorrect)
+
+
+merge_states = list()
+for file in correct:
+    f = open("./data/states\\"+file+".csv", "r", encoding = 'utf-8')
+    rdr = list(csv.reader(f))
+    # print(file, list(rdr)[:3])
+    for i in range(len(rdr)):
+        rdr[i].insert(0, file)
+    merge_states += rdr
+    f.close()
+
+for i in merge_states:
+    with open("data/states/merge_states.csv", 'w') as f:
+        temp = str(i[0])
+        for _ in i[1:]:
+            temp += ","+_
+        f.write(temp+"\n")
