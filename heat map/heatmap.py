d76fe7f40568210049f032d01322ffae2826e820
double J, T <50047351+Powering111@users.noreply.github.com>
2022-07-07 19:12:12 +0900

    연도별 히트맵 완성

--- heat map/heatmap.py
+++ heat map/heatmap.py
@@ -5,26 +5,30 @@ import pandas as pd
 
 class FoliumTest:
     def __init__(self):
-        self._context = './data/'
+        self._context = 'heat map/data/'
 
     def hook(self):
         self.show_map()
 
-    def show_map(self):
+    def show_map(self, year):
         state_geo = self._context + "us-states.json"
-        state_unemployment = self._context + 'US.csv'
+        state_unemployment = self._context + f'{year}.csv'
         state_data = pd.read_csv(state_unemployment)
         m = folium.Map(location=[37, -102], zoom_start=5)
         m.choropleth(
             geo_data = state_geo,
             name='choropleth',
             data=state_data,
-            columns=['State','Unemployment'],
+            columns=['State',' Unemployment'],
             key_on = 'feature.id',
-            fill_color = 'YlGn',
+            fill_color = 'YlOrBr',
             fill_opacity=0.7,
             line_opacity=0.2,
             legent_name='Unemployment Rate (%)'
         )
         folium.LayerControl().add_to(m)
-        m.save('./USA.html')
+        m.save(f'heat map/map/USA bee colony-{year}.html')
+
+for year in range(1987, 2018):
+    FoliumTest().show_map(year)
+    print(f'{year} done')
--- heat map/make_data_for_heatmap.py
+++ heat map/make_data_for_heatmap.py
@@ -30,33 +30,38 @@ state_symbol = {
     'Montana': 'MT',
     'Nebraska': 'NE',
     'Nevada': 'NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
+    'New hampshire': 'NH',
+    'New jersey': 'NJ',
+    'New mexico': 'NM',
+    'New york': 'NY',
+    'North carolina': 'NC',
+    'North dakota': 'ND',
     'Ohio': 'OH',
     'Oklahoma': 'OK',
     'Oregon': 'OR',
     'Pennsylvania': 'PA',
-    'Rhode Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
+    'Rhode island': 'RI',
+    'South carolina': 'SC',
+    'South dakota': 'SD',
     'Tennessee': 'TN',
     'Texas': 'TX',
     'Utah': 'UT',
     'Vermont': 'VT',
     'Virginia': 'VA',
     'Washington': 'WA',
-    'West Virginia': 'WV',
+    'West virginia': 'WV',
     'Wisconsin': 'WI',
     'Wyoming': 'WY'
 }
 
-for year in [1987, 1997, 2007, 2017]:
+for year in range(1987, 2018):
     with open(f'heat map/data/{year}.csv', 'a') as f:
         f.write('State, Unemployment\n')
-        for line in data.loc:
-            if line['Year'] == year:
-                f.write(f'{state_symbol[line["State"]]},{line["Value"]}\n')
+        try:
+            for line in data.loc:
+                if line['Year'] == year:
+                    state = line['State'].lower()
+                    state = state[0].upper() + state[1:]
+                    f.write(f'{state_symbol[state]},{line["Value"]}\n')
+        except:
+            pass
