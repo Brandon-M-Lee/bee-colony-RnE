7ae2603086af5e73bbdebb23e270dd2eded0d32e
Gabriel-Dropout <lhs0831lkh@naver.com>
2022-07-08 23:08:54 +0900

    피어슨만으로 수정
    ㅇ

--- python/choice_corr.py
+++ python/choice_corr.py
@@ -52,25 +52,29 @@ useful_feature_rms = {
 }
 for state in os.listdir('data/states'):
     texts = list()
+    corr_ = ""
     print(state)
     try:
         with open(f'data/correlation/{state}.txt', 'r', encoding='utf-8') as f:
             string = f.readlines()
             
             for i in string:
-                if i.find("상관 관계") != -1:
+                if i.find("상관 관계") != -1: #첫번째 줄 넘기기
                     continue
                 if i.find('상관계수') != -1 or i.find('tau') != -1:
                     texts.append(i)
+                    corr_ = i.split()[0]
                     continue
                 temp = i.split()
-                all_feature_rms[i[ : i.index(":")]] += float(temp[-1])**2
-                all_n[i[ : i.index(":")]] += 1
+                if corr_ == "pearson": #피어슨만 RMS 계산
+                    all_feature_rms[i[ : i.index(":")]] += float(temp[-1])**2
+                    all_n[i[ : i.index(":")]] += 1
                 try:
                     if abs(float(temp[-1])) > 0.7:
                         texts.append(i)
-                        useful_feature_rms[i[ : i.index(":")]] += float(temp[-1])**2
-                        useful_n[i[ : i.index(":")]] += 1
+                        if corr_ == "pearson": #피어슨만 RMS 계산
+                            useful_feature_rms[i[ : i.index(":")]] += float(temp[-1])**2
+                            useful_n[i[ : i.index(":")]] += 1
                 except:
                     pass
         with open('data/correlation/upper 0.7/' + state + '.txt', 'w', encoding='utf-8') as f:
@@ -80,10 +84,10 @@ for state in os.listdir('data/states'):
         print(f'{state} is NOT found')
         
 with open('data/correlation/useful data/useful_feature_rms.txt', 'w', encoding='utf-8') as f:
-    f.write("useful feature rms\n")
+    f.write("pearson useful feature rms\n")
     for i in useful_feature_rms:
         f.write(f'{i} : {rms(useful_feature_rms[i]/useful_n[i])}\n')
         
-    f.write('all_feature_rms\n')
+    f.write('pearson all feature rms\n')
     for i in all_feature_rms:
         f.write(f'{i} : {rms(all_feature_rms[i]/all_n[i])}\n')
