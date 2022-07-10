a3dbcfafff3a727bee47e46040b53bfb94dd715e
double J, T <50047351+Powering111@users.noreply.github.com>
2022-07-10 17:56:41 +0900

    꿀벌 군집 수 그리기

--- python/bee_change.py
+++ python/bee_change.py
@@ -1,2 +1,15 @@
+import os
+os.system('pip install -r requirements.txt')
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+path = 'data/states'
+for state in os.listdir(path):
+    bee_data = pd.read_csv(f'{path}/{state}/{state}_bee.csv')
+    plt.title(f'Changes in the number of bee colonies in {state}')
+    plt.xlabel('Year')
+    plt.ylabel('Number of bee colonies')
+    plt.plot(list(reversed(list(bee_data['Year']))), list(reversed(list(bee_data[' Value']))))
+    plt.savefig(f'{path}/{state}/{state}_bee.png')
+    plt.clf()
