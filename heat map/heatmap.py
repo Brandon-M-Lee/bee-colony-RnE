925188b2d4218a73f3849f434c11be0a42d60d4a
Brandon-M-Lee <minjae.py@gmail.com>
2022-07-10 22:58:11 +0900

    피어슨 양음 히트맵 완성

--- heat map/heatmap.py
+++ heat map/heatmap.py
@@ -10,25 +10,30 @@ class FoliumTest:
     def hook(self):
         self.show_map()
 
-    def show_map(self, year):
+    def show_map(self, climate):
         state_geo = self._context + "us-states.json"
-        state_unemployment = self._context + f'{year}.csv'
+        state_unemployment = self._context + f"pearson/{climate}.csv"
         state_data = pd.read_csv(state_unemployment)
         m = folium.Map(location=[37, -102], zoom_start=5)
         m.choropleth(
             geo_data = state_geo,
             name='choropleth',
             data=state_data,
-            columns=['State',' Unemployment'],
+            columns=['State','Unemployment'],
             key_on = 'feature.id',
-            fill_color = 'YlOrBr',
+            fill_color = 'BrBG',
             fill_opacity=0.7,
             line_opacity=0.2,
             legent_name='Unemployment Rate (%)'
         )
         folium.LayerControl().add_to(m)
-        m.save(f'heat map/map/USA bee colony-{year}.html')
+        m.save(f'heat map/map/html/pearson/{climate}.html')
 
-for year in range(1987, 2018):
-    FoliumTest().show_map(year)
-    print(f'{year} done')
+# for year in range(1987, 2018):
+#     FoliumTest().show_map(year)
+#     print(f'{year} done')
+
+for climate in ['temp_high', 'temp_low', 'dew_high', 'dew_low', 'wind_high', 'wind_low', 'rain', 'pressure_high', 'pressure_low']:
+    FoliumTest().show_map(climate)
+    print(f'{climate} done')
+    
--- heat map/make_data_for_heatmap.py
+++ heat map/make_data_for_heatmap.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv('./merge_states.csv')
+
 state_symbol = {
     'Alabama': 'AL',
     'Alaska': 'AK',
@@ -54,14 +54,115 @@ state_symbol = {
     'Wyoming': 'WY'
 }
 
-for year in range(1987, 2018):
-    with open(f'heat map/data/{year}.csv', 'a') as f:
-        f.write('State, Unemployment\n')
-        try:
-            for line in data.loc:
-                if line['Year'] == year:
-                    state = line['State'].lower()
-                    state = state[0].upper() + state[1:]
-                    f.write(f'{state_symbol[state]},{line["Value"]}\n')
-        except:
-            pass
+def colony_by_year():
+    data = pd.read_csv('./merge_states.csv')
+    for year in range(1987, 2018):
+        with open(f'heat map/data/year/{year}.csv', 'a') as f:
+            f.write('State, Unemployment\n')
+            try:
+                for line in data.loc:
+                    if line['Year'] == year:
+                        state = line['State'].lower()
+                        state = state[0].upper() + state[1:]
+                        f.write(f'{state_symbol[state]},{line["Value"]}\n')
+            except:
+                pass
+
+def pearson_plus_minus():
+    with open('data/correlation/pearson_plus_minus.txt', 'r', encoding='utf-8') as f:
+        plus = 1000000
+        minus = 10
+        lines = f.readlines()
+        with open('heat map/data/pearson/temp_high.csv', 'a') as f_temp_high:
+            f_temp_high.write('State,Unemployment\n')
+            temp_high_plus = eval(lines[2])
+            temp_high_minus = eval(lines[29])
+            for state in temp_high_plus:
+                state = state[0] + state.lower()[1:]
+                f_temp_high.write(f'{state_symbol[state]},{plus}\n')
+            for state in temp_high_minus:
+                state = state[0] + state.lower()[1:]
+                f_temp_high.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/temp_low.csv', 'a') as f_temp_low:
+            f_temp_low.write('State,Unemployment\n')
+            temp_low_plus = eval(lines[5])
+            temp_low_minus = eval(lines[32])
+            for state in temp_low_plus:
+                state = state[0] + state.lower()[1:]
+                f_temp_low.write(f'{state_symbol[state]},{plus}\n')
+            for state in temp_low_minus:
+                state = state[0] + state.lower()[1:]
+                f_temp_low.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/dew_high.csv', 'a') as f_dew_high:
+            f_dew_high.write('State,Unemployment\n')
+            dew_high_plus = eval(lines[8])
+            dew_high_minus = eval(lines[35])
+            for state in dew_high_plus:
+                state = state[0] + state.lower()[1:]
+                f_dew_high.write(f'{state_symbol[state]},{plus}\n')
+            for state in dew_high_minus:
+                state = state[0] + state.lower()[1:]
+                f_dew_high.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/dew_low.csv', 'a') as f_dew_low:
+            f_dew_low.write('State,Unemployment\n')
+            dew_low_plus = eval(lines[11])
+            dew_low_minus = eval(lines[38])
+            for state in dew_low_plus:
+                state = state[0] + state.lower()[1:]
+                f_dew_low.write(f'{state_symbol[state]},{plus}\n')
+            for state in dew_low_minus:
+                state = state[0] + state.lower()[1:]
+                f_dew_low.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/wind_high.csv', 'a') as f_wind_high:
+            f_wind_high.write('State,Unemployment\n')
+            wind_high_plus = eval(lines[14])
+            wind_high_minus = eval(lines[41])
+            for state in wind_high_plus:
+                state = state[0] + state.lower()[1:]
+                f_wind_high.write(f'{state_symbol[state]},{plus}\n')
+            for state in wind_high_minus:
+                state = state[0] + state.lower()[1:]
+                f_wind_high.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/wind_low.csv', 'a') as f_wind_low:
+            f_wind_low.write('State,Unemployment\n')
+            wind_low_plus = eval(lines[17])
+            wind_low_minus = eval(lines[44])
+            for state in wind_low_plus:
+                state = state[0] + state.lower()[1:]
+                f_wind_low.write(f'{state_symbol[state]},{plus}\n')
+            for state in wind_low_minus:
+                state = state[0] + state.lower()[1:]
+                f_wind_low.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/rain.csv', 'a') as f_rain:
+            f_rain.write('State,Unemployment\n')
+            rain_plus = eval(lines[20])
+            rain_minus = eval(lines[47])
+            for state in rain_plus:
+                state = state[0] + state.lower()[1:]
+                f_rain.write(f'{state_symbol[state]},{plus}\n')
+            for state in rain_minus:
+                state = state[0] + state.lower()[1:]
+                f_rain.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/pressure_high.csv', 'a') as f_pressure_high:
+            f_pressure_high.write('State,Unemployment\n')
+            pressure_high_plus = eval(lines[23])
+            pressure_high_minus = eval(lines[50])
+            for state in pressure_high_plus:
+                state = state[0] + state.lower()[1:]
+                f_pressure_high.write(f'{state_symbol[state]},{plus}\n')
+            for state in pressure_high_minus:
+                state = state[0] + state.lower()[1:]
+                f_pressure_high.write(f'{state_symbol[state]},{minus}\n')
+        with open('heat map/data/pearson/pressure_low.csv', 'a') as f_pressure_low:
+            f_pressure_low.write('State,Unemployment\n')
+            pressure_low_plus = eval(lines[26])
+            pressure_low_minus = eval(lines[53])
+            for state in pressure_low_plus:
+                state = state[0] + state.lower()[1:]
+                f_pressure_low.write(f'{state_symbol[state]},{plus}\n')
+            for state in pressure_low_minus:
+                state = state[0] + state.lower()[1:]
+                f_pressure_low.write(f'{state_symbol[state]},{minus}\n')
+
+if __name__ == '__main__':
+    pearson_plus_minus()
