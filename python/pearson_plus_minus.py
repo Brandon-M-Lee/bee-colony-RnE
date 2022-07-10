e3f20a4c759aed340d944ea78a256c241082d051
double J, T <50047351+Powering111@users.noreply.github.com>
2022-07-10 21:41:04 +0900

    피어슨 플마 무슨 주까지

--- python/pearson_plus_minus.py
+++ python/pearson_plus_minus.py
@@ -76,21 +76,48 @@ for state in os.listdir('data/correlation'):
             else:
                 if idx%9 == 0:
                     temp_high_minus[0] += 1
+                    temp_high_minus.append(state[:-4])
                 elif idx%9 == 1:
                     temp_low_minus[0] += 1
+                    temp_low_minus.append(state[:-4])
                 elif idx%9 == 2:
                     dew_high_minus[0] += 1
+                    dew_high_minus.append(state[:-4])
                 elif idx%9 == 3:
                     dew_low_minus[0] += 1
+                    dew_low_minus.append(state[:-4])
                 elif idx%9 == 4:
                     wind_high_minus[0] += 1
+                    wind_high_minus.append(state[:-4])
                 elif idx%9 == 5:
                     wind_low_minus[0] += 1
+                    wind_low_minus.append(state[:-4])
                 elif idx%9 == 6:
                     rain_minus[0] += 1
+                    rain_minus.append(state[:-4])
                 elif idx%9 == 7:
                     pressure_high_minus[0] += 1
+                    pressure_high_minus.append(state[:-4])
                 elif idx%9 == 8:
                     pressure_low_minus[0] += 1
+                    pressure_low_minus.append(state[:-4])
             idx += 1
 
+print('temp_high_plus:', temp_high_plus)
+print('temp_high_minus:', temp_high_minus)
+print('temp_low_plus:', temp_low_plus)
+print('temp_low_minus:', temp_low_minus)
+print('dew_high_plus:', dew_high_plus)
+print('dew_high_minus:', dew_high_minus)
+print('dew_low_plus:', dew_low_plus)
+print('dew_low_minus:', dew_low_minus)
+print('wind_high_plus:', wind_high_plus)
+print('wind_high_minus:', wind_high_minus)
+print('wind_low_plus:', wind_low_plus)
+print('wind_low_minus:', wind_low_minus)
+print('rain_plus:', rain_plus)
+print('rain_minus:', rain_minus)
+print('pressure_high_plus:', pressure_high_plus)
+print('pressure_high_minus:', pressure_high_minus)
+print('pressure_low_plus:', pressure_low_plus)
+print('pressure_low_minus:', pressure_low_minus)
